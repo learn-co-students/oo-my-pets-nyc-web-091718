4564e37cad55e3423e1918e38ad6bf97acdb3eec
OGonzalezNYC <OGonzalezNYC@Gmail.com>
2018-09-20 22:43:54 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,70 +27,56 @@ class Owner
      end
    end
 
-     def say_species
-       "I am a #{@species}."
-     end
+   def say_species
+      "I am a #{@species}."
+   end
+
+    def buy_fish(fish_name)
+      fish = Fish.new(fish_name)
+      @pets[:fishes] << fish
+    end
+
+    def buy_cat(cat_name)
+      cat = Cat.new(cat_name)
+      @pets[:cats] << cat
+    end
+
+    def buy_dog(dog_name)
+      dog = Dog.new(dog_name)
+      @pets[:dogs] << dog
+    end
+
+    def walk_dogs
+      @pets[:dogs] = @pets[:dogs].collect do |dog|
+        dog.mood="happy"
+      end
+    end
+
+    def play_with_cats
+      @pets[:cats] = @pets[:cats].collect do |cat|
+        cat.mood="happy"
+      end
+    end
+
+    def feed_fish
+      @pets[:fishes] = @pets[:fishes].collect do |fish|
+        fish.mood="happy"
+      end
+    end
+
+    def sell_pets
+      @pets = @pets.collect do |animal_species_symbol, array_of_animal_species|
+        array_of_animal_species.each do |pet|
+          pet.mood = "nervous"
+        end
+      end
+      @pets = {fishes: [], cats: [], dogs: []}
+    end
 
 
-     def buy_fish(fish_name)
-       fish = Fish.new(fish_name)
-       @pets[:fishes] << fish
-     end
-
-
-     def buy_cat(cat_name)
-       cat = Cat.new(cat_name)
-       @pets[:cats] << cat
-     end
-
-
-     def buy_dog(dog_name)
-       dog = Dog.new(dog_name)
-       @pets[:dogs] << dog
-     end
-
-
-     def walk_dogs
-       @pets[:dogs] = @pets[:dogs].collect do |dog|
-         dog.mood="happy"
-       end
-     end
-
-
-     def play_with_cats
-       @pets[:cats] = @pets[:cats].collect do |cat|
-         cat.mood="happy"
-       end
-     end
-
-
-     def feed_fish
-       @pets[:fishes] = @pets[:fishes].collect do |fish|
-         fish.mood="happy"
-       end
-     end
-
-
-     # def pets_empty?
-     #   if @pets
-     #
-     #   else
-     #     false
-     # end
-     #
-     def sell_pets
-       @pets = @pets.collect do |animal_species_symbol, array_of_animal_species|
-         array_of_animal_species.each do |pet|
-           pet.mood = "nervous"
-         end
-       end
-       @pets = {fishes: [], cats: [], dogs: []}
-     end
-
-
-     def list_pets
-       "I have #{@pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
-     end
+    def list_pets
+      "I have #{@pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+    end
 #{}"I have 2 fish, 3 dog(s), and 1 cat(s)."
 end
 # sell_pets
